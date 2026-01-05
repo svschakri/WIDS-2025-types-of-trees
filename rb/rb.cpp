8cc2bb877d40c38b513baa69e929d8964879bb46
svschakri <svschakri777@gmail.com>
2026-01-05 14:25:19 +0530

    Implement Red-Black Tree insertion and rotations

--- rb/rb.cpp
+++ rb/rb.cpp
@@ -24,9 +24,35 @@ ptr RedBlackTree::insert(int data)
 // you may assume start is not nullptr
 void RedBlackTree::insert(ptr start, ptr newnodePtr)
 {
+	// 1 represents red
 	// choose direction
-	
+	while(true){
+		if(start->data>newnodePtr->data){
+			if(start->left==nullptr){
+				start->left=newnodePtr;
+				newnodePtr->parent=start;
+				newnodePtr->color= 1 ;
+				newnodePtr->left=nullptr;
+				newnodePtr->right=nullptr;
+				break;
+			}
+			start=start->left;
+		}	
+		else{
+			if(start->right==nullptr){
+				start->right=newnodePtr;
+				newnodePtr->parent=start;
+				newnodePtr->color= 1 ;
+				newnodePtr->left=nullptr;
+				newnodePtr->right=nullptr;
+				break;
+			}
+			start=start->right;
+		}
+	}
 	// recurse down the tree
+
+	fixup(newnodePtr);
 
 	return;
 }
@@ -49,21 +75,135 @@ void RedBlackTree::printRBT(ptr start, const std::string& prefix, bool isLeftChi
 // of the passed node
 void RedBlackTree::rightrotate(ptr loc)
 {
-	
+	ptr lloc=loc->left;
+	if (lloc==nullptr) return;
+	ptr ploc=loc->parent;
+	ptr rlloc=lloc->right;
+	if (ploc==nullptr){
+		lloc->parent=nullptr;
+		root = lloc;
+		lloc->right=loc;
+		if (rlloc != nullptr){
+			rlloc->parent = loc;
+		}
+		loc->parent=lloc;
+		loc->left=rlloc;
+	}
+	else if (ploc->left==loc){
+		ploc->left=lloc;
+		lloc->right=loc;
+		lloc->parent=ploc;
+		if (rlloc != nullptr){
+			rlloc->parent = loc;
+		}
+		loc->parent=lloc;
+		loc->left=rlloc;
+	}
+	else if(ploc->right==loc){
+		ploc->right=lloc;
+		lloc->right=loc;
+		lloc->parent=ploc;
+		if (rlloc != nullptr){
+			rlloc->parent = loc;
+		}
+		loc->parent=lloc;
+		loc->left=rlloc;
+	}
 }
 
 // Function performing left rotation
 // of the passed node
 void RedBlackTree::leftrotate(ptr loc)
 {
-	
+	ptr rloc=loc->right;
+	if (rloc==nullptr) return;
+	ptr ploc=loc->parent;
+	ptr lrloc=rloc->left;
+	if (ploc==nullptr){
+		rloc->parent=nullptr;
+		root = rloc;
+		rloc->left=loc;
+		if (lrloc != nullptr){
+			lrloc->parent = loc;
+		}
+		loc->parent=rloc;
+		loc->right=lrloc;
+	}
+	else if (ploc->left==loc){
+		ploc->left=rloc;
+		rloc->left=loc;
+		rloc->parent=ploc;
+		if (lrloc != nullptr){
+			lrloc->parent = loc;
+		}
+		loc->parent=rloc;
+		loc->right=lrloc;
+	}
+	else if(ploc->right==loc){
+		ploc->right=rloc;
+		rloc->left=loc;
+		rloc->parent=ploc;
+		if (lrloc != nullptr){
+			lrloc->parent = loc;
+		}
+		loc->parent=rloc;
+		loc->right=lrloc;
+	}
 }
 
 // This function fixes violations
 // caused by RBT insertion
 void RedBlackTree::fixup(ptr loc)
 {
-	
+	ptr ploc=loc->parent;
+	if(ploc==nullptr) {
+		loc->color=0;
+		return ;
+	}
+	// if ploc I mean parent of loc is root then it will be of black color any way that's why i coded like this with out checking pploc == root
+
+	if (ploc->color==0){
+		return;
+	}
+	ptr pploc=ploc->parent;
+	if (pploc==nullptr){
+		ploc->color=0;
+		return;
+	}
+	ptr sploc;
+	if (pploc->left==ploc){
+		sploc=pploc->right;
+	}
+	else if(pploc->right==ploc){
+		sploc=pploc->left;
+	}
+	if (sploc!=nullptr && sploc->color==1){
+		pploc->color=1;
+		sploc->color=0;
+		ploc->color=0;
+		RedBlackTree::fixup(pploc);
+		return;
+	}
+		if(pploc->right==ploc&&ploc->left==loc){
+			rightrotate(ploc);
+			RedBlackTree::fixup(ploc);
+			return;
+		}
+		else if(pploc->left==ploc&&ploc->right==loc){
+			leftrotate(ploc);
+			RedBlackTree::fixup(ploc);
+			return;
+		}
+	if(pploc->left==ploc){
+		rightrotate(pploc);
+	}
+	else{
+		leftrotate(pploc);
+	}
+	int temp=pploc->color;
+	pploc->color=ploc->color;
+	ploc->color=temp;
+	return;
 }
 
 // Function to print inorder traversal
